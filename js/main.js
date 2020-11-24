d9505f9cb6821cb3485d7de4b6b5cc945302c6e7
rubinashakya <rubinashakya68@gmail.com>
2020-11-24 11:30:47 +0545

    linear bacjground fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
-jQuery(document).ready(function() {
-    jQuery('.multiple-select').select2();
+$(document).ready(function() {
+    $('.multiple-select').select2();
+    $('#signup-carousel').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.password-strength-input').keyup(function(){
+        let len = this.value.length;
+        
+        if (len === 0) {
+        $('.password-strength_bar').each(function() {
+            $(this).removeClass('active')
+        });
+        } else if (len > 0 && len <= 4) {
+            $('.password-strength__bar--1').addClass('active');
+            $('.password-strength__bar--2').removeClass('active');
+            $('.password-strength__bar--3').removeClass('active');
+            $('.password-strength__bar--4').removeClass('active');
+            $('.password-strength__bar--5').removeClass('active');
+        } else if (len > 4 && len <= 6) {
+            $('.password-strength__bar--2').addClass('active');
+            $('.password-strength__bar--3').removeClass('active');
+            $('.password-strength__bar--4').removeClass('active');
+            $('.password-strength__bar--4').removeClass('active');
+        } else if (len > 6 && len <= 8) {
+            $('.password-strength__bar--3').addClass('active');
+            $('.password-strength__bar--4').removeClass('active');
+            $('.password-strength__bar--5').removeClass('active');
+        } else if (len > 8 && len <= 10) {
+            $('.password-strength__bar--4').addClass('active');
+            $('.password-strength__bar--5').removeClass('active');
+        } else {
+            $('.password-strength__bar').each(function() {
+                $(this).addClass('active');
+            });
+        } 
+    });
 });
